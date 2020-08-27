5f27a4bc6f2658bffa20640e6a439eea1829d38e
Hoax142 <hyoh0606@gmail.com>
2020-08-27 18:11:07 +0900

    Do it! 스위프트 프로그래밍 3단원

--- 3.ImageView/ViewController.swift
+++ 3.ImageView/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isZoom = false // 확대 여부를 나타내는 bool 변수
+    var imgOn: UIImage? // 켜진 전구 이미지가 있는 UIImage 타입의 변수
+    var imgOff: UIImage? // 꺼진 전구 이미지가 있는 UIImage 타입의 변수
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png") // imgOn에 "lamp_on.png" 할당
+        imgOff = UIImage(named: "lamp_off.png") // imgOff에 "lamp_off.png" 할당
+        
+        imageView.image = imgOn
     }
-
+    
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale: CGFloat = 1.5
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if isZoom { // 확대 상태
+            newWidth = imageView.frame.width / scale // imageView의 넓이 값 / scale
+            newHeight = imageView.frame.height / scale // imageView의 높이 값 / scale
+            btnResize.setTitle("확대", for: .normal)
+        } else { // 축소 상태
+            newWidth = imageView.frame.width * scale // imageView의 넓이 값 * scale
+            newHeight = imageView.frame.height * scale // imageView의 높이 값 * scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        
+        imageView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn { // switch on 상태
+            imageView.image = imgOn
+        } else { // switch oㄹㄹ 상태
+            imageView.image = imgOff
+        }
+    }
+    
 
 }
 
